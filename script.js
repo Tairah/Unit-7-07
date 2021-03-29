2541480339409f24e7069bafef5e6e56890e4488
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-29 13:41:46 +0000

    Added to script.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let firstNum = 0
 let secondNum = 0
 let counter = 0
 let answer = 0
+let negative = false
+
 
 function calculate () {
   answer = 0
@@ -11,10 +13,25 @@ function calculate () {
   secondNum = document.getElementById('second').value
   firstNum = parseInt(firstNum)
   secondNum = parseInt(secondNum)
+  answer = 0
+  negative = false
 
+  if (firstNum < 0) {
+    negative = !negative
+    firstNum = Math.abs(firstNum)
+  }
+
+  if (secondNum < 0) {
+    negative = !negative
+    secondNum = Math.abs(secondNum)
+  }
   for (counter = 0; counter < firstNum; counter++) {
     answer = answer + secondNum
      }
+
+  if (negative) {
+    answer = 0 - answer
+  }
      document.getElementById('answer').innerHTML = answer
   }
   
